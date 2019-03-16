4656482ab1b90a03e0031d7c79082fb819e12b26
Matteo Velardi <matteovelardi79@gmail.com>
2019-03-16 23:23:35 +0100

    First commit: Client and Program

---
+++ Client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    public class Client
+    {
+        IPEndPoint serverEndPoint;
+        Socket socket;
+
+        public Client(string address, int port)
+        {
+            serverEndPoint = new IPEndPoint(IPAddress.Parse(address), port);
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        }
+
+        public void Process()
+        {
+            while (true)
+            {
+                string message = Console.ReadLine();
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                socket.SendTo(data, serverEndPoint);
+
+                byte[] response = new byte[data.Length];
+                int rlen = socket.Receive(response);
+                Console.WriteLine(new string(Encoding.UTF8.GetChars(response, 0, response.Length)));
+            }
+        }
+    }
+}
